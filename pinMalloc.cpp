61b5ddb7ae1036411c2031ea0a016f217486a2be
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-04-16 06:45:23 -0400

    Template

---
+++ pinMalloc.cpp
@@ -0,0 +1,24 @@
+//#include "header.h"
+#include "pin.H"
+
+#include <iostream>
+#include<fstream>
+using std::cerr;
+using std::ofstream;
+using std::ios;
+using std::string;
+using std::endl;
+
+ofstream outFile; /*Setup for the outfile*/
+
+
+int main(int argc, char** argv){
+
+
+
+
+
+
+
+  return 0;
+}
