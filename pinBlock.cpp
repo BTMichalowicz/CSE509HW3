2a9ceac591f8448fd4f204984a226f26f41956c5
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-04-16 14:58:49 -0400

    Working slowly

--- pinBlock.cpp
+++ pinBlock.cpp
@@ -13,6 +13,24 @@ using std::endl;
 
 ofstream outFile; /*Setup for the outfile*/
 
+/*Command Line Switched*/
+KOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", 
+	 "o", "pinBlock.out", "specify trace file name");
+
+
+
+/*Usage*/
+static INT32 Usage(){
+
+  cerr << "This pin tool collects data on every basic block executed\n";
+  cerr << "Meaning, any control flow jump causes the end of a block, and post-jump";
+  cerr << " begins another block\n";
+
+  cerr << KNOB_BASE::StringKnobSummary() << endl;
+    return -1;
+}
+
+
 
 int main(int argc, char** argv){
 
