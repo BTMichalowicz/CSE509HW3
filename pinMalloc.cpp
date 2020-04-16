76896bc327c7cba801448e2f2fb7fb5f60b8f032
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-04-16 13:14:34 -0400

    pinMalloc works. Onto pinBlock

--- pinMalloc.cpp
+++ pinMalloc.cpp
@@ -3,9 +3,11 @@
 
 #include <iostream>
 #include<fstream>
+using std::hex;
 using std::cerr;
 using std::ofstream;
 using std::ios;
+using std::dec;
 using std::string;
 using std::endl;
 
@@ -16,10 +18,22 @@ static unsigned long long totalMalloc=0; /*Keeping track of malloc-ed bytes*/
 #define FREE "_free"
 #else
 #define MALLOC "malloc"
-#define FREE "free
+#define FREE "free"
 #endif
 
 
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "pinMalloc.out", "specify trace file name");
+
+
+/*Set up argbefore and argafter, writing to the given output file*/
+VOID Arg1Before(CHAR* name, ADDRINT size){
+  totalMalloc+=size;
+  outFile << name << "(" << size << ")" <<endl;
+}
+
+VOID MallocAfter(ADDRINT returnVal){
+  outFile << " returns " << returnVal << endl;
+}
 
 /*Set up the Instrumentation Routing*/
 VOID Image(IMG img, VOID* v){
@@ -34,20 +48,33 @@ VOID Image(IMG img, VOID* v){
 		  IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
 		  IARG_END);
 	 RTN_InsertCall(mallocRtn, IPOINT_AFTER, (AFUNPTR)MallocAfter,
-		  IARG_FUNCRET_EXITPOINT_VALUE, IARGEND);
+		  IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
 	 RTN_Close(mallocRtn);
   }
 
   /* Find the Free Function*/
   RTN freeRtn = RTN_FindByName(img, FREE);
+  if(RTN_Valid(freeRtn)){
+	 RTN_Open(freeRtn);
+	 /*Set up free to print values*/
+	 RTN_InsertCall(freeRtn, IPOINT_BEFORE, (AFUNPTR)Arg1Before,
+		  IARG_ADDRINT, FREE, IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+		  IARG_END);
+	 RTN_Close(freeRtn);
+  }
 
 
 }
 
-void Fini(INT32 code, VOID* v){ outFile.close();} /*We're done here*/
+void Fini(INT32 code, VOID* v){
+  outFile << dec;
+  outFile<< "Total Byted Allocated: " << totalMalloc << endl;
+  outFile.close();
+
+} /*We're done here*/
 
 INT32 Usage(){
-  cerr << "This tool produces traces of calls to malloc and keeps track of the number of bytes malloced" << end;
+  cerr << "This tool produces traces of calls to malloc and keeps track of the number of bytes malloced" << endl;
   cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
   return -1;
 }
@@ -55,14 +82,14 @@ INT32 Usage(){
 int main(int argc, char** argv){
   /*Setup*/
   PIN_InitSymbols();
-  if(PIN_INIT(argc,argv)){
+  if(PIN_Init(argc,argv)){
 	 return Usage();
   }
 
   /*Write to files just in case file descriptors are close*/
-  outFile.open(KnobOutputfile.Value().c_str());
+  outFile.open(KnobOutputFile.Value().c_str());
   outFile << hex;
-  outFile.set(ios::showbase);
+  outFile.setf(ios::showbase);
 
   /*Register an image to be called to instrument fxn*/
   IMG_AddInstrumentFunction(Image,0);
