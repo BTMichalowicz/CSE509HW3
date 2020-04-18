9724839b043cd5bb3a1ab0e823eb8ec57d3cc7d9
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-04-17 22:47:17 -0400

    btraceApp.cpp

---
+++ btraceApp.cpp
@@ -0,0 +1,14 @@
+#include "pin.H"
+#include<iostream>
+#include<fstream>
+#include<map>
+#include<unistd.h>
+using std::hex;
+using std::dec;;
+using std::cerr;
+using std::ofstream;
+using std::ios;
+using std::string;
+using std::endl;
+
+/**No outfile. Gonna print off function names for this*/
